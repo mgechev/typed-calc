90d8f7bd6058727f0640fc8ba10f157dc0bdaf83
mgechev <mgechev@gmail.com>
2017-02-22 21:57:33 -0800

    docs: add comments to eval

--- eval.js
+++ eval.js
@@ -4,19 +4,32 @@ const { ASTNodes } = require('./ast');
 const SymbolTable = new SymbolTableImpl();
 
 const Eval = ast => {
+  // The empty program evaluates to null.
   if (!ast) {
     return null;
   }
+
+  // The literals evaluate to their values.
   if (ast.type === ASTNodes.Literal) {
     return ast.value;
+
+  // The variables evaluate to the values
+  // that are bound to them in the SymbolTable.
   } else if (ast.type === ASTNodes.Identifier) {
     return SymbolTable.lookup(ast.name);
+
+  // if-then-else evaluates to the expression of the
+  // then clause if the condition is true, otherwise
+  // to the value of the else clause.
   } else if (ast.type === ASTNodes.Condition) {
     if (Eval(ast.condition)) {
       return Eval(ast.then);
     } else {
       return Eval(ast.el);
     }
+
+  // The abstraction creates a new context of execution
+  // and registers it's argument in the SymbolTable.
   } else if (ast.type === ASTNodes.Abstraction) {
     const scope = new Scope();
     return x => {
@@ -24,8 +37,16 @@ const Eval = ast => {
       SymbolTable.push(scope);
       return Eval(ast.body);
     };
+
+  // IsZero checks if the evaluated value of its
+  // expression equals `0`.
   } else if (ast.type === ASTNodes.IsZero) {
     return Eval(ast.expression) === 0;
+
+  // The arithmetic operations manipulate the value
+  // of their corresponding expressions:
+  // - `succ` adds 1.
+  // - `pred` substracts 1.
   } else if (ast.type === ASTNodes.Arithmetic) {
     const op = ast.operator;
     const val = Eval(ast.expression);
@@ -35,6 +56,12 @@ const Eval = ast => {
       case 'pred':
         return val - 1;
     }
+
+  // The application evaluates to:
+  // - Evaluation of the left expression.
+  // - Evaluation of the right expression.
+  // Application of the evaluation of the left expression over
+  // the evaluated right expression.
   } else if (ast.type === ASTNodes.Application) {
     const l = Eval(ast.left);
     const r = Eval(ast.right);
@@ -44,3 +71,4 @@ const Eval = ast => {
 };
 
 module.exports.Eval = Eval;
+
